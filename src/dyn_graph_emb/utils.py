cd2f98af483310912cddaa83445d62bd74df6f61
Suchanuch Piriyasatit <suchanuch.pir@gmail.com>
2024-12-09 14:51:48 +0800

    add text file utils

--- src/dyn_graph_emb/utils.py
+++ src/dyn_graph_emb/utils.py
@@ -2,3 +2,12 @@ def save_list_to_file(lst, file_path):
     with open(file_path, 'w') as f:
         for elt in lst:
             f.write(f"{elt}\n")
+
+
+def read_list_from_file(file_path):
+    lst = []
+    with open(file_path, 'r') as f:
+        for line in f.readlines():
+            lst.append(line.rstrip())
+    return lst
+
