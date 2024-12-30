0201267a7a2592343fd52153fd5c6a8d82756458
Suchanuch Piriyasatit <suchanuch.pir@gmail.com>
2024-12-30 18:39:05 +0800

    add doc2vec param

--- src/dyn_graph_emb/ts_model.py
+++ src/dyn_graph_emb/ts_model.py
@@ -60,7 +60,9 @@ class DynConnectomeEmbed:
         return documents
 
     def run_doc2vec(self, documents):
-        model = Doc2Vec(vector_size=self.embedding_dim, window=self.window_size, epochs=self.epochs, workers=self.workers)
+        model = Doc2Vec(vector_size=self.embedding_dim, window=self.window_size,
+                        epochs=self.epochs, workers=self.workers,
+                        dm_mean=self.config['dm_mean'], dm_concat=self.config['dm_concat'])
         model.build_vocab(documents)
         model.train(documents, total_examples=model.corpus_count, epochs=model.epochs)
         save_path = os.path.join(self.save_dir, 'model.model')
